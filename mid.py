a3d46164e1b4374e88b87bce3ad70972e270564a
nerallaMani <134263995+nerallaMani@users.noreply.github.com>
2023-11-03 15:06:11 +0530

    Add files via upload

---
+++ mid.py
@@ -0,0 +1,22 @@
+#cricket balls to how many overs!
+def over(balls):
+	if balls%6==0:
+		return balls/6
+	return balls%6/10+balls//6
+print(over(2400))
+	
+import time as t
+
+def decorator(func):
+	def inner(name):
+		start=t.time()
+		func(name)
+		print("you successfully logged in.")
+		print("it takes time", t.time()-start,"secs")
+	return inner
+
+@decorator
+def greet(name):
+	print(f"hi {name}")
+
+greet("manikanta")
