e911f38fb2a47c254ce9feb3dd493a87541a0b78
nerallaMani <134263995+nerallaMani@users.noreply.github.com>
2023-11-22 14:14:53 +0530

    Update Find_second_low_marks_student.py

--- Find_second_low_marks_student.py
+++ Find_second_low_marks_student.py
@@ -1,3 +1,21 @@
+'''
+Given the names and grades for each student in a class of  students, store them in a nested list and print the name(s) of any student(s) 
+having the second lowest grade.
+
+Note: If there are multiple students with the second lowest grade, order their names alphabetically and print each name on a new line.
+
+The ordered list of scores is , so the second lowest score is . There are two students with that score: . 
+Ordered alphabetically, the names are printed as:
+
+example:
+python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+output:
+Berry
+Harry
+
+'''
+
 records = []
 for _ in range(int(input())):
     name = input()
